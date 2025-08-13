08b43926750f6423b6f8e04b7a6eeb5ab0a74600
Riccardo Zuliani <zuliani.riccardo.98@gmail.com>
2025-08-13 14:33:03 +0200

    added system identification and more parameter updates including robust descent

--- examples/random_linear_models/generate_printout.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-result_name = '/home/riccardoz/Documents/Git/BP-MPC_improving_closed_loop_MPC_performance/.results/gd_CE_2025_07_09_17_12_43_random_linear_models_NX_4_POLE_MAG_-5.0_to_1.0_N_MODELS_50_NOISE.pkl'
-
-with open(result_name, 'rb') as f:
-    result = pickle.load(f)
-
-print(*result['printout'], sep='\n')
